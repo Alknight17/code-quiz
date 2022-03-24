11a00c8298eaf6b9ce10a02f833299bb522e11dd
Alknight17 <knightalex17@gmail.com>
2022-03-24 17:42:33 -0400

    Add timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,7 @@ nextButton.addEventListener('click', () => {
 })
 
 function startQuiz() {
+    startTimer();
     startButton.classList.add('hide');
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
@@ -135,6 +136,27 @@ const questions = [
             ]
             },
 ]
+
+function startTimer(){
+    var counter = 200;
+    setInterval(function() {
+      counter--;
+      if (counter >= 0) {
+        span = document.getElementById("countdown");
+        span.innerHTML = counter;
+      }
+      if (counter === 0) {
+          alert('sorry, out of time');
+          clearInterval(counter);
+      }
+    }, 1000);
+  }
+
+  function start()
+  {
+      document.getElementById("count").style="color:green;";
+      startTimer();
+  };
 
 function saveScore() {
 
