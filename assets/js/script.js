8717134af4ff21710e094ac8951438bcc393fd89
Alknight17 <knightalex17@gmail.com>
2022-03-24 17:03:39 -0400

    Add quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn') 
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-btns')
@@ -7,6 +8,10 @@ const answerButtonsElement = document.getElementById('answer-btns')
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startQuiz)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    nextQuestion()
+})
 
 function startQuiz() {
     startButton.classList.add('hide');
@@ -17,24 +22,120 @@ function startQuiz() {
 }
 
 function nextQuestion() {
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
     questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', chooseAnswer)
+        answerButtonsElement.appendChild(button)
+    })
 }
 
-function chooseAnswer() {
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
+} 
 
+function chooseAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+    nextButton.classList.remove('hide')
+}
+
+function setStatusClass ( element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')       
+    } else {
+      element.classList.add('wrong')  
+    }
+}
+
+function clearStatusClass(element){
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
 
 // array for questions
 const questions = [
     { 
-     question: "What is 2 + 2 ?",
+     question: "Which directory command creates a new file?",
      answers: [
-        {text: '4', correct: true},
-        {text: '20', correct: false}
+        {text: 'cd ..', correct: false},
+        {text: 'mkdir', correct: false},
+        {text: 'touch', correct: true},
+        {text: 'pwd', correct: false},
      ]
-    }
+    },
+    {
+    question: "Choose the syntax that would create an ordered list with numbers",
+    answers: [
+        {text: '<p>', correct:false},
+        {text: '<ol>', correct:true},
+        {text: '<ul>', correct:false},
+        {text: '<a>', correct:false},
+    ]
+    },
+    {
+    question: "Which of these displays the proper flow of the CSS box model from outside to inside",
+    answers: [
+        {text: 'content > padding > border > margin', correct:false},
+        {text: 'margin > border > padding > content', correct:true},
+        {text: 'padding > border > margin > content', correct:false},
+        {text: 'margin > padding > content > border', correct:false},
+        ]
+        },
+    {
+    question: "Using dot notation, select the proper syntax for <section class='hero'>",
+    answers: [
+        {text: '.hero ', correct:true},
+        {text: '#hero', correct:false},
+        {text: '-hero', correct:false},
+        {text: '*hero', correct:false},
+    ]
+    },
+    {
+    question: "Which of these elements allows for a more responsive web page that can adapt to different screen sizes?",
+    answers: [
+        {text: 'justify-content', correct:false},
+        {text: 'flex-grow', correct:false},
+        {text: 'space-around', correct:false},
+        {text: 'mediaquery', correct:true},
+        ]
+        },   
+        {
+    question: "What is it called when a string and a variable are combined?",
+    answers: [
+        {text: 'string theory', correct:false},
+        {text: 'string variables', correct:false},
+        {text: 'string concantenation', correct:true},
+        {text: 'string cheese', correct:false},
+            ]
+            },
 ]
+
+function saveScore() {
+
+}
